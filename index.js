b649e6ae29f823fb8a23a3f5d48764d72965b3e5
Haze Booth <isnt@haze.cool>
2023-10-06 19:00:02 -0400

    Add binaries for x86 linux, x86 macOS, and arm64 macOS

--- index.js
+++ index.js
@@ -2,56 +2,64 @@ import { visit, SKIP } from "unist-util-visit";
 import { rehype } from "rehype";
 import { createRequire } from "node:module";
 import { h } from "hastscript";
+import os from 'node:os';
 import stringByteSlice from "string-byte-slice";
 const require = createRequire(import.meta.url);
-const core = require("./dist/index.node");
+
+const lookup = {
+    "darwin-x64": "./dist/x86_64-apple-darwin.node",
+    "linux-x64": "./dist/x86_64-unknown-linux-gnu.node",
+    "darwin-arm64": "./dist/aarch64-apple-darwin.node",
+}
+
+const core = require(lookup[`${os.platform()}-${os.arch()}`] ?? "./dist/index.node");
 
 const exampleScopeMap = {
-  javascript: "source.js",
-  sh: "source.bash",
-  xml: "source.xml",
+    javascript: "source.js",
+    sh: "source.bash",
+    xml: "source.xml",
 };
 
 const isCodeBlockElement = (node) => node.tagName === "code";
 export default function rehypeTreeSitter(options) {
-  if (options === undefined)
-    throw new Error("Need to provide `options.treeSitterGrammarRoot`");
-  if (options.treeSitterGrammarRoot === undefined)
-    throw new Error("Need to provide `options.treeSitterGrammarRoot`");
-  return function (tree) {
-    visit(tree, isCodeBlockElement, (node, index, parent) => {
-      if (Object.keys(node.properties).length === 0) return;
-      const code = node.children[0].value;
-      const inputLanguage = node.properties.className[0];
-      node.children = [];
-      const highlightStack = [];
-      const language = inputLanguage.split("-")[1];
-      core.driver(
-        options.treeSitterGrammarRoot,
-        (options.scopeMap || exampleScopeMap)[language] ?? "",
-        language,
-        code,
-        (event) => {
-          if (event.source !== undefined) {
-            const sourceChunk = stringByteSlice(
-              code,
-              Number(event.source.start),
-              Number(event.source.end)
-            );
-            if (highlightStack.length === 0) {
-              node.children.push({ type: "text", value: sourceChunk });
-            } else {
-              node.children.push(
-                h("span", { class: highlightStack.join(" ") }, sourceChunk)
-              );
-            }
-          } else if (event.highlightStart !== undefined) {
-            highlightStack.push(event.highlightStart.highlightName);
-          } else if (event === "HighlightEnd") {
-            highlightStack.pop();
-          }
-        }
-      );
-    });
-  };
+    if (options === undefined)
+	throw new Error("Need to provide `options.treeSitterGrammarRoot`");
+    if (options.treeSitterGrammarRoot === undefined)
+	throw new Error("Need to provide `options.treeSitterGrammarRoot`");
+    return function (tree) {
+	visit(tree, isCodeBlockElement, (node, index, parent) => {
+	    if (Object.keys(node.properties).length === 0) return;
+	    const code = node.children[0].value;
+	    const inputLanguage = node.properties.className[0];
+	    node.children = [];
+	    const highlightStack = [];
+	    const language = inputLanguage.split("-")[1];
+	    core.driver(
+		options.treeSitterGrammarRoot,
+		(options.scopeMap || exampleScopeMap)[language] ?? "",
+		language,
+		code,
+		(event) => {
+		    if (event.source !== undefined) {
+			const sourceChunk = stringByteSlice(
+			    code,
+			    Number(event.source.start),
+			    Number(event.source.end)
+			);
+			if (highlightStack.length === 0) {
+			    node.children.push({ type: "text", value: sourceChunk });
+			} else {
+			    node.children.push(
+				h("span", { class: highlightStack.join(" ") }, sourceChunk)
+			    );
+			}
+		    } else if (event.highlightStart !== undefined) {
+			highlightStack.push(event.highlightStart.highlightName);
+		    } else if (event === "HighlightEnd") {
+			highlightStack.pop();
+		    }
+		}
+	    );
+	});
+    };
 }
