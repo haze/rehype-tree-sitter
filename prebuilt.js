91224e5bdea4103908393458478e1282d2285b3f
Haze Booth <isnt@haze.cool>
2023-10-06 20:03:32 -0400

    add prebuilt.js -_-

---
+++ prebuilt.js
@@ -0,0 +1,15 @@
+import os from "node:os";
+
+const lookup = {
+    "darwin-x64": "./dist/x86_64-apple-darwin.node",
+    "linux-x64": "./dist/x86_64-unknown-linux-gnu.node",
+    "darwin-arm64": "./dist/aarch64-apple-darwin.node",
+}
+
+export function getBinaryPath() {
+    return lookup[`${os.platform()}-${os.arch()}`] ?? "./dist/index.node";
+}
+
+export function hasPrebuiltBinary() {
+    return lookup[`${os.platform()}-${os.arch()}`] !== undefined;
+}
